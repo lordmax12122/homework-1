69baf980c415598bab4de4118363f738e23f07e8
lordmax12122 <116733872+lordmax12122@users.noreply.github.com>
2024-09-29 12:13:56 +0300

    homework 1

---
+++ js.js
@@ -0,0 +1,35 @@
+const age = 13;
+console.log(age);
+
+const name = "Max";
+console.log(name);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = "колобок повісився";
+console.log(myString);
+
+const myNumber = 10 + 10;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+
+const name2 = prompt("введіть своє імя")
+alert("вітаю," + name2 + " ви виграли приз")
+
+const isConfirmed = confirm("підвердити дію????????????")
+if (confirm) {
+    alert("ви підтвертили")
+} else {
+    alert("не підтверджено:(((((")
+}
+
+alert("ей! це небезпечно!!");
+let isConfirmed2= confirm("підтвердити дію??? " + name2 + " це небезпечно!!! ");
+if (confirm) {
+  alert("ти підтвердив, але навіщо????");
+} else {
+  alert("ти непідтвердив");
+}
